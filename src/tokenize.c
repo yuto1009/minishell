03ac0993cdb0d84971660b0237e978cf0ff9f26d
yuto1009 <bmwb71009@gmail.com>
2023-11-14 23:34:28 +0900

    update: get rid of unnecessary debug code

--- src/tokenize.c
+++ src/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: yutoendo <yutoendo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 18:57:26 by yutoendo          #+#    #+#             */
-/*   Updated: 2023/11/14 21:51:14 by yutoendo         ###   ########.fr       */
+/*   Updated: 2023/11/14 23:33:13 by yutoendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,45 +80,6 @@ t_token *tokenize_operator(char **line)
     return (new_token(operator, TK_OPERATOR));
 } 
 
-// char *remove_quotes(const char *word) {
-
-//     const size_t result_size = ft_strlen(word) + 1;
-//     char *result;
-//     size_t i;
-//     size_t j;
-    
-//     if (word == NULL) 
-//         return NULL;
-//     result = (char *)malloc(result_size * sizeof(char));
-//     if (result == NULL) 
-//         fatal_error("malloc error");
-//     i = 0;
-//     j = 0;
-//     while (word[i] != '\0') {
-//         if (word[i] == SINGLE_QUOTE || word[i] == DOUBLE_QUOTE) 
-//         {
-//             const char current_quote = word[i++];
-//             while (word[i] != '\0' && word[i] != current_quote) 
-//             {
-//                 result[j++] = word[i++];
-//             }
-//             if (word[i] == '\0') 
-//             {
-//                 free(result);
-//                 minishell_error("unclosed quote");
-//             }
-//             i++; // 閉じクオートをスキップ
-//         } 
-//         else 
-//         {
-//             result[j++] = word[i++];
-//         }
-//     }
-//     free((void *)word);
-//     result[j] = '\0';
-//     return result;
-// }
-
 t_token *tokenize_word(char **line)
 {
     const size_t word_size = ft_strlen(*line) + 1;
@@ -131,7 +92,6 @@ t_token *tokenize_word(char **line)
         fatal_error("malloc error");
     i = 0;
     j = 0;
-    // printf("start of the line: %s\n", *line);    // DEBUG
     while ((*line)[i] != '\0' && is_metacharacter((*line)[i]) == false)
     {
         if ((*line)[i] == SINGLE_QUOTE || (*line)[i] == DOUBLE_QUOTE)
@@ -140,14 +100,12 @@ t_token *tokenize_word(char **line)
             i++;    // クオートをスキップ
             while ((*line)[i] != '\0' && (*line)[i] != current_quote)
             {
-                // printf("(*line)[i]: %c\n", (*line)[i]);    // DEBUG
                 word[j] = (*line)[i];
                 i++;
                 j++;
             }
             if ((*line)[i] == '\0')
             {
-                // printf("word: %s\n", word);    // DEBUG
                 free(word);
                 minishell_error("unclosed quote");
             }
@@ -188,7 +146,6 @@ t_token *tokenize(char *line)
                 new = tokenize_word(&line);
             }
         }
-        // printf("new token: %s<-\n", new->str);    // DEBUG
         if (head == NULL)
         {
             head = new;
--- tmp.c
+++ tmp.c
@@ -10,124 +10,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 
-// char *remove_quotes(char *word)
-// {
-//     const int result_size = ft_strlen(word) + 1;
-//     char *result;
-//     char *trimmed;
-//     size_t i;
-//     size_t str_size;
-
-//     result = (char *)ft_calloc(result_size, sizeof(char));
-//     if (result == NULL)
-//         exit(EXIT_FAILURE);
-//     i = 0;
-//     while(word[i] != '\0')
-//     {
-//         str_size = 0;
-//         if (word[i] != '\0' && word[i] == SINGLE_QUOTE)
-//         {
-//             i++;
-//             while (word[i] != '\0' && word[i] != SINGLE_QUOTE)
-//             {
-//                 str_size++;
-//                 i++;
-//             }
-//             if (word[i] == '\0' || word[i] != SINGLE_QUOTE)
-//             {
-//                 exit(EXIT_FAILURE);
-//             }
-//             else
-//             {
-//                 trimmed = ft_substr(word, i - str_size, str_size);
-//                 if (trimmed == NULL)
-//                     exit(EXIT_FAILURE);
-//                 ft_strlcat(result, trimmed, result_size);
-//                 free(trimmed);
-//             }
-//         }
-//         str_size = 0;
-//         if (word[i] != '\0' && word[i] == DOUBLE_QUOTE)
-//         {
-//             i++;
-//             while (word[i] != '\0' && word[i] != DOUBLE_QUOTE)
-//             {
-//                 str_size++;
-//                 i++;
-//             }
-//             if (word[i] == '\0' || word[i] != DOUBLE_QUOTE)
-//             {
-//                 exit(EXIT_FAILURE);
-//             }
-//             else
-//             {
-//                 trimmed = ft_substr(word, i - str_size, str_size);
-//                 if (trimmed == NULL)
-//                     exit(EXIT_FAILURE);
-//                 ft_strlcat(result, trimmed, result_size);
-//                 free(trimmed);
-//             }
-//         }
-//         printf("%s\n", result);
-//     }
-//     // minishellの場合　free(word); した方がよくね？
-//     return (result);
-// }
-
-char *remove_quotes(const char *word) {
-
-    size_t result_size = strlen(word) + 1;
-    char *result = (char *)malloc(result_size * sizeof(char));
-    size_t i;
-    size_t j;
-    
-
-    if (word == NULL) 
-        return NULL;
-    if (result == NULL) 
-        return NULL;
-    i = 0;
-    j = 0;
-    while (word[i] != '\0') {
-        if (word[i] == SINGLE_QUOTE || word[i] == DOUBLE_QUOTE) 
-        {
-            const char current_quote = word[i++];
-            while (word[i] != '\0' && word[i] != current_quote) 
-            {
-                result[j++] = word[i++];
-            }
-            if (word[i] == '\0') 
-            {
-                free(result);
-                return NULL;    // errre_exit?
-            }
-            i++; // Skip the closing quote
-        } else 
-        {
-            result[j++] = word[i++];
-        }
-    }
-    // free(word) minishellの場合
-    result[j] = '\0'; // Null-terminate the string
-    return result;
-}
-
-int main() 
+//リダイレクト関数
+int main(int argc , char *argv[])
 {
-    // char input[] = "\'\'\"\"\"\'Hello\'\"\"\"\'\'";
-    char input[] = "\"\'H\'\"e\"l\"o\"\'o\'\"";
-    // Original: "'H'"e"l"o"'o'"
-    // Modified: 'H'elo'o'
-    char *output = remove_quotes(input);
-
-    if (output != NULL) {
-        printf("Original: %s\n", input);
-        printf("Modified: %s\n", output);
-        free(output);
-    } else {
-        printf("Memory allocation failed.\n");
-    }
-
+    int fd;
+    fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    dup2(fd, 1);
+    close(fd);// fdはもう使わないので閉じる
+    printf("Hello World\n");
     return 0;
 }

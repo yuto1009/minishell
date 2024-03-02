2ce695fe8b2650004a6282952a5d21ad4422068e
Yuto Endo <yuendo@c1r22s9.42tokyo.jp>
2024-03-02 14:03:23 +0900

    a

--- src/expand.c
+++ src/expand.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yutoendo <yutoendo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 16:00:29 by yuendo            #+#    #+#             */
-/*   Updated: 2024/02/24 16:43:36 by yutoendo         ###   ########.fr       */
+/*   Updated: 2024/03/02 13:02:18 by yuendo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,10 +58,8 @@ static void expand_token(t_token *token)
     }
 }
 
-static char *remove_quotes(char **str)
+static char *remove_double_quotes(char **str)
 {
-    // シングルクオートとダブルクオートをbashの挙動の通りに除去
-    // tokeninze.cのtokenize_word関数にコードあり
     const size_t word_size = ft_strlen(*str) + 1;
     char *word;
     size_t i;
@@ -74,11 +72,10 @@ static char *remove_quotes(char **str)
     j = 0;
     while ((*str)[i] != '\0' && is_metacharacter((*str)[i]) == false)
     {
-        if ((*str)[i] == SINGLE_QUOTE || (*str)[i] == DOUBLE_QUOTE)
+        if ((*str)[i] == DOUBLE_QUOTE)
         {
-            const char current_quote = (*str)[i];
             i++;    // クオートをスキップ
-            while ((*str)[i] != '\0' && (*str)[i] != current_quote)
+            while ((*str)[i] != '\0' && (*str)[i] != DOUBLE_QUOTE)
             {
                 word[j] = (*str)[i];
                 i++;
@@ -99,8 +96,95 @@ static char *remove_quotes(char **str)
         }
     }
     word[j] = '\0';
+    free(*str);
     return (word);
 }
+
+static char *remove_single_quotes(char **str)
+{
+    const size_t word_size = ft_strlen(*str) + 1;
+    char *word;
+    size_t i;
+    size_t j;
+
+    word = (char *)ft_calloc(word_size, sizeof(char));
+    if (word == NULL)
+        fatal_error("malloc error");
+    i = 0;
+    j = 0;
+    while ((*str)[i] != '\0' && is_metacharacter((*str)[i]) == false)
+    {
+        if ((*str)[i] == SINGLE_QUOTE)
+        {
+            i++;    // クオートをスキップ
+            while ((*str)[i] != '\0' && (*str)[i] != SINGLE_QUOTE)
+            {
+                word[j] = (*str)[i];
+                i++;
+                j++;
+            }
+            if ((*str)[i] == '\0')
+            {
+                free(word);
+                minishell_error("unclosed quote");
+            }
+            i++;    // 閉じクオートスキップ
+        }
+        else
+        {
+            word[j] = (*str)[i];
+            i++;
+            j++;
+        }
+    }
+    word[j] = '\0';
+    free(*str);
+    return (word);
+}
+
+// static char *remove_quotes(char **str)
+// {
+//     // シングルクオートとダブルクオートをbashの挙動の通りに除去
+//     // tokeninze.cのtokenize_word関数にコードあり
+//     const size_t word_size = ft_strlen(*str) + 1;
+//     char *word;
+//     size_t i;
+//     size_t j;
+
+//     word = (char *)ft_calloc(word_size, sizeof(char));
+//     if (word == NULL)
+//         fatal_error("malloc error");
+//     i = 0;
+//     j = 0;
+//     while ((*str)[i] != '\0' && is_metacharacter((*str)[i]) == false)
+//     {
+//         if ((*str)[i] == SINGLE_QUOTE || (*str)[i] == DOUBLE_QUOTE)
+//         {
+//             const char current_quote = (*str)[i];
+//             i++;    // クオートをスキップ
+//             while ((*str)[i] != '\0' && (*str)[i] != current_quote)
+//             {
+//                 word[j] = (*str)[i];
+//                 i++;
+//                 j++;
+//             }
+//             if ((*str)[i] == '\0')
+//             {
+//                 free(word);
+//                 minishell_error("unclosed quote");
+//             }
+//             i++;    // 閉じクオートスキップ
+//         }
+//         else
+//         {
+//             word[j] = (*str)[i];
+//             i++;
+//             j++;
+//         }
+//     }
+//     word[j] = '\0';
+//     return (word);
+// }
 
 // getenv関数は未知の環境変数に対してNULLを返す
 static bool is_unknown_env(t_token *token)
@@ -114,30 +198,29 @@ void expand(t_node *node)
     while(node != NULL)
     {   
         // tokenを昇る
-        t_token *tmp;
-        tmp = node->token;
-        while(tmp != NULL && tmp->kind !=TK_EOF)
+        t_token *tmp_token;
+        tmp_token = node->token;
+        while(tmp_token != NULL && tmp_token->kind !=TK_EOF)
         {
+            tmp_token->str = remove_double_quotes(&tmp_token->str);
             // tokenがWORD && ドルサインがあれば、展開に進む
-            if(tmp->kind == TK_WORD && ft_strchr(tmp->str, DOLLAR_SIGN))
+            if(tmp_token->kind == TK_WORD && ft_strchr(tmp_token->str, DOLLAR_SIGN))
             {
-                expand_token(tmp);  // トークンを展開へ
-                if (is_unknown_env(tmp) == true)    // 指定の環境変数が存在しない場合、そのトークンはなかったことにする
+                expand_token(tmp_token);  // トークンを展開へ
+                if (is_unknown_env(tmp_token) == true)    // 指定の環境変数が存在しない場合、そのトークンはなかったことにする
                 {
-                    const t_token *void_token = tmp;
-                    if (tmp->prev != NULL && tmp->next != NULL)
-                        tmp->prev->next = tmp->next;
-                    tmp = tmp->next; 
+                    const t_token *void_token = tmp_token;
+                    if (tmp_token->prev != NULL && tmp_token->next != NULL)
+                        tmp_token->prev->next = tmp_token->next;
+                    tmp_token = tmp_token->next; 
                     free((void *)void_token);
                     continue;
                 }
             }
             // トークンからクオートを除去
-            printf("token->str: %s\n", tmp->str);
-            const char *trimmed_str = remove_quotes(&tmp->str);
-            free(tmp->str);
-            tmp->str = (char *)trimmed_str;
-            tmp = tmp->next;
+            printf("token->str: %s\n", tmp_token->str);
+            tmp_token->str = remove_single_quotes(&tmp_token->str);
+            tmp_token = tmp_token->next;
         }
         node = node->next;
     }

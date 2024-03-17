9cb8e00e8adbd3db55f1ec55ab83cb5088177620
Yuto Endo <yuendo@c1r15s8.42tokyo.jp>
2024-03-17 12:08:08 +0900

    ビルトインのenv関数を更新

--- built_in/builtin_env.c
+++ built_in/builtin_env.c
@@ -3,64 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
+/*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 18:45:42 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/01/08 19:44:25 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/03/17 12:05:47 by yuendo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/built_in.h"
 
-t_var *create_kv(char *environ)
+// t_var *create_kv(char *environ)
+// {
+//   char **kv_str;
+//   t_var *new;
+//   kv_str = ft_split(environ, '=');
+//   if(!kv_str)
+//     return NULL;
+//   new = (t_var *)ft_calloc(1, sizeof(t_var));
+//   new->name = kv_str[0];
+//   new->value = kv_str[1];
+//   free(kv_str);
+//   return new;
+// }
+
+// t_var *env_to_var(char **environ)
+// {
+//   int i ;
+//   i = 0;
+//   t_var *last;
+//   t_var *start;
+//   t_var *new;
+//   while(environ[i] != NULL)
+//   {
+//     new = create_kv(environ[i]);
+//     if(last!=NULL)
+//       last->next = new;
+//     last = new;
+//     if(start == NULL)
+//       start = new;
+//     // printf("%s\n", environ[i]);
+//     i++;
+//   }
+//   return start;
+// }
+
+void mini_env(t_var *env_map)
 {
-  char **kv_str;
-  t_var *new;
-  kv_str = ft_split(environ, '=');
-  if(!kv_str)
-    return NULL;
-  new = (t_var *)ft_calloc(1, sizeof(t_var));
-  new->key = kv_str[0];
-  new->value = kv_str[1];
-  free(kv_str);
-  return new;
-}
+  //  extern char **environ; // 環境変数へのポインタ
+  //  t_var *var;
+  //   (void)argv;
 
-
-t_var *env_to_var(char **environ)
-{
-  int i ;
-  i = 0;
-  t_var *last;
-  t_var *start;
-  t_var *new;
-  while(environ[i] != NULL)
-  {
-    new = create_kv(environ[i]);
-    if(last!=NULL)
-      last->next = new;
-    last = new;
-    if(start == NULL)
-      start = new;
-    // printf("%s\n", environ[i]);
-    i++;
-  }
-  return start;
-}
-
-int mini_env(char **argv)
-{
-   extern char **environ; // 環境変数へのポインタ
-   t_var *var;
-    (void)argv;
-
-    var = env_to_var(environ);
-    while(var != NULL)
-    {
-      printf("%s=%s\n", var->key, var->value);
-      var = var->next;
-    }
-    return 0;
+  //   var = env_to_var(environ);
+  //   while(var != NULL)
+  //   {
+  //     printf("%s=%s\n", var->key, var->value);
+  //     var = var->next;
+  //   }
+  printf("%s", get_env_list(env_map));
 }
 
 // int main()

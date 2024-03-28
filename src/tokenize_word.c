2947a92c9b4f84e0b49d388ef8a8efd818fa8c34
Yuto Endo <yuendo@c1r11s1.42tokyo.jp>
2024-03-28 16:07:55 +0900

    all ok

--- src/tokenize_word.c
+++ src/tokenize_word.c
@@ -3,55 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   tokenize_word.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
+/*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 11:44:21 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/03/24 11:44:47 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/03/28 16:07:28 by yuendo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static void process_quoted_word(char **line, char *word, size_t *i, size_t *j)
+static void	process_quoted_word(char **line, char *word, size_t *i, size_t *j)
 {
-    char current_quote;
-    current_quote = (*line)[(*i)++];
-    word[(*j)++] = current_quote;
+	char	current_quote;
 
-    while ((*line)[*i] != '\0') {
-        word[(*j)++] = (*line)[*i];
-        if (current_quote == (*line)[*i]) {
-            (*i)++;
-            break;
-        }
-        (*i)++;
-    }
+	current_quote = (*line)[(*i)++];
+	word[(*j)++] = current_quote;
+	while ((*line)[*i] != '\0')
+	{
+		word[(*j)++] = (*line)[*i];
+		if (current_quote == (*line)[*i])
+		{
+			(*i)++;
+			break ;
+		}
+		(*i)++;
+	}
 }
 
-static t_token *create_word_token(char *word)
+static t_token	*create_word_token(char *word)
 {
-    return new_token(word, TK_WORD);
+	return (new_token(word, TK_WORD));
 }
 
-t_token *tokenize_word(char **line)
+t_token	*tokenize_word(char **line)
 {
-    char *word;
-    size_t i = 0;
-    size_t j = 0;
+	char	*word;
+	size_t	i;
+	size_t	j;
 
-    word = (char *)ft_calloc(ft_strlen(*line) + 1, sizeof(char));
-    if (word == NULL)
-        fatal_error("malloc error");
-
-    while ((*line)[i] != '\0' && !is_metacharacter((*line)[i])) {
-        if ((*line)[i] == SINGLE_QUOTE || (*line)[i] == DOUBLE_QUOTE) {
-            process_quoted_word(line, word, &i, &j);
-        } else {
-            word[j++] = (*line)[i++];
-        }
-    }
-    word[j] = '\0';
-    *line += i;
-
-    return create_word_token(word);
+	i = 0;
+	j = 0;
+	word = (char *)ft_calloc(ft_strlen(*line) + 1, sizeof(char));
+	if (word == NULL)
+		fatal_error("malloc error");
+	while ((*line)[i] != '\0' && !is_metacharacter((*line)[i]))
+	{
+		if ((*line)[i] == SINGLE_QUOTE || (*line)[i] == DOUBLE_QUOTE)
+		{
+			process_quoted_word(line, word, &i, &j);
+		}
+		else
+		{
+			word[j++] = (*line)[i++];
+		}
+	}
+	word[j] = '\0';
+	*line += i;
+	return (create_word_token(word));
 }

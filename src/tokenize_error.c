8fb09e8db9b6d2cf033aaea01cc72e3b11b8c114
Yuto Endo <yuendo@c1r11s1.42tokyo.jp>
2024-03-28 16:11:08 +0900

    tokenize_error.c norminette all ok

--- src/tokenize_error.c
+++ src/tokenize_error.c
@@ -3,39 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   tokenize_error.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yutoendo <yutoendo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:33:09 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2024/03/23 17:12:41 by yutoendo         ###   ########.fr       */
+/*   Updated: 2024/03/28 16:10:51 by yuendo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int tokenize_error(t_token *token)
+int	tokenize_error(t_token *token)
 {
-    if(token == NULL || token->str == NULL)
-    return 0;
-    if(token->kind ==TK_OPERATOR)
-        return syntax_error_exit(token->str);
-    if((token->kind == TK_REDIRECTION)&& count_token_len(token) == 1)
-        return  syntax_error_exit("newline");
-    
-    while(token->kind!= TK_EOF && token->next->kind!=TK_EOF)
-    {
-        // if(unsupported_variable(token->next) < 0)
-        //     return 127;
-        if(token->kind == TK_OPERATOR && token->next->kind == TK_OPERATOR)
-            return syntax_error_exit(token->next->str);            
-        else if(token->kind == TK_REDIRECTION && token->next->kind == TK_REDIRECTION)
-            return syntax_error_exit(token->next->str);
-        else if(token->kind == TK_REDIRECTION && token->next->kind == TK_OPERATOR)
-            return syntax_error_exit(token->next->str);
-        token = token->next;
-    }
-    if((token->kind == TK_REDIRECTION)&&token->next->kind == TK_EOF)
-        return syntax_error_exit("newline");
-    else if((token->kind == TK_OPERATOR)&&token->next->kind == TK_EOF)
-        return syntax_error_exit(token->str);
-    return 0;
+	if (token == NULL || token->str == NULL)
+		return (0);
+	if (token->kind == TK_OPERATOR)
+		return (syntax_error_exit(token->str));
+	if ((token->kind == TK_REDIRECTION) && count_token_len(token) == 1)
+		return (syntax_error_exit("newline"));
+	while (token->kind != TK_EOF && token->next->kind != TK_EOF)
+	{
+		if (token->kind == TK_OPERATOR && token->next->kind == TK_OPERATOR)
+			return (syntax_error_exit(token->next->str));
+		else if (token->kind == TK_REDIRECTION
+			&& token->next->kind == TK_REDIRECTION)
+			return (syntax_error_exit(token->next->str));
+		else if (token->kind == TK_REDIRECTION
+			&& token->next->kind == TK_OPERATOR)
+			return (syntax_error_exit(token->next->str));
+		token = token->next;
+	}
+	if ((token->kind == TK_REDIRECTION) && token->next->kind == TK_EOF)
+		return (syntax_error_exit("newline"));
+	else if ((token->kind == TK_OPERATOR) && token->next->kind == TK_EOF)
+		return (syntax_error_exit(token->str));
+	return (0);
 }

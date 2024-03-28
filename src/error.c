8399c4a85f154a5edb882f2c2303bca04f0bd3a4
Yuto Endo <yuendo@c1r11s1.42tokyo.jp>
2024-03-28 16:56:00 +0900

    error.c norminette all ok

--- src/error.c
+++ src/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
+/*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 15:22:40 by yutoendo          #+#    #+#             */
-/*   Updated: 2024/03/26 17:32:39 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2024/03/28 16:55:36 by yuendo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,23 @@ int	minishell_error(char *message)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putendl_fd(message, STDERR_FILENO);
-    return (1);
-	// write(1,"\n",1);
-	// exit(MINISHELL_ERROR);
+	return (1);
 }
+
 int	cd_error(char *cmd)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd("cd: ", STDERR_FILENO);
 	ft_putstr_fd(cmd, STDERR_FILENO);
 	ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
-    return (1);
+	return (1);
 }
 
 int	syntax_error_exit(char *token_str)
 {
-	const char	*join_str = ft_strjoin(token_str, "'");
+	const char	*join_str;
 
+	join_str = ft_strjoin(token_str, "'");
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd("syntax error near unexpected token '", STDERR_FILENO);
 	ft_putstr_fd((char *)join_str, STDERR_FILENO);
@@ -48,8 +48,9 @@ int	syntax_error_exit(char *token_str)
 
 int	unsupported_token_msg(char *str)
 {
-	const char *join_str = ft_strjoin("sorry this command is unsupported : ",
+	const char	*join_str = ft_strjoin("sorry this command is unsupported : ",
 			str);
+
 	ft_putstr_fd((char *)join_str, STDERR_FILENO);
 	write(1, "\n", 1);
 	return (-1);

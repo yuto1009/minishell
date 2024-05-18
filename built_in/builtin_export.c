ff18d9187ae8324af92202b074c7aeb837a02111
Yuto Endo <yuendo@c4r2s11.42tokyo.jp>
2024-05-18 18:20:44 +0900

    ノーム整理済み

--- built_in/builtin_export.c
+++ built_in/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 17:10:24 by kyoshida          #+#    #+#             */
-/*   Updated: 2024/05/18 17:53:02 by yuendo           ###   ########.fr       */
+/*   Updated: 2024/05/18 18:19:05 by yuendo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,8 @@ bool	is_env_exists(t_var *env_map, char *name)
 	return (false);
 }
 
-void handle_env_operation(t_var *env_map, int env_operator, char *env_name, char *env_value)
+void	handle_env_operation(t_var *env_map, int env_operator, char *env_name,
+		char *env_value)
 {
 	if (env_operator == ASSIGN_OP)
 	{
@@ -54,82 +55,43 @@ void handle_env_operation(t_var *env_map, int env_operator, char *env_name, char
 		append_env(env_map, env_name, env_value);
 }
 
-int	builtin_export(char **args, t_var *env_map)
+static int	process_env_argument(char *arg, t_var *env_map)
 {
-	int			i;
+	const int	env_operator = find_env_operator(arg);
 	char		*env_name;
 	char		*env_value;
-	int	env_operator;
+
+	if (env_operator == INVALID_OP)
+		return (0);
+	env_name = trim_env_name(arg, env_operator);
+	if (!env_name && is_identifier(arg))
+		return (0);
+	env_value = trim_env_value(arg);
+	if (!is_identifier(env_name) || (!env_name && !is_identifier(arg))
+		|| env_operator == INVALID_OP)
+	{
+		free(env_name);
+		free(env_value);
+		g_status = GENERAL_ERRORS;
+		return (command_error(arg, "export"));
+	}
+	handle_env_operation(env_map, env_operator, env_name, env_value);
+	return (1);
+}
+
+int	builtin_export(char **args, t_var *env_map)
+{
+	int	i;
 
 	i = 1;
 	while (args[i] != NULL)
 	{
-		env_operator = find_env_operator(args[i]);
-		if (env_operator == INVALID_OP)
+		if (!process_env_argument(args[i], env_map))
 		{
-			// printf("args[%d]: %s check --A\n", i, args[i]);
 			i++;
-			continue;
+			continue ;
 		}
-		env_name = trim_env_name(args[i], env_operator);
-		if (!env_name && is_identifier(args[i]))
-		{
-			// printf("args[%d]: %s check --B\n", i, args[i]);
-			return (0);
-		}
-		env_value = trim_env_value(args[i]);
-		if (!is_identifier(env_name) || (!env_name && !is_identifier(args[i]))
-			|| env_operator == INVALID_OP)
-		{
-			// printf("args[%d]: %s check --C\n", i, args[i]);
-			free(env_name);
-			free(env_value);
-			g_status = GENERAL_ERRORS;
-			return (command_error(args[i], "export"));
-		}
-		printf("args[%d]: %s check --OK\n", i, args[i]);
-		handle_env_operation(env_map, env_operator, env_name, env_value);
 		i++;
 	}
 	return (g_status);
 }
-
-// int	builtin_export(char **args, t_var *env_map)
-// {
-// 	int			i;
-// 	char		*env_name;
-// 	char		*env_value;
-// 	int	env_operator;
-
-// 	i = 1;
-// 	while (args[i] != NULL)
-// 	{
-// 		env_operator = find_env_operator(args[i]);
-// 		if (env_operator == INVALID_OP && find_env_operator(args[i + 1]))
-// 		{
-// 			printf("args[%d]: %s check --A\n", i, args[i]);
-// 			g_status = GENERAL_ERRORS;
-// 			command_error(args[i + 1], "export");
-// 			i += 2;
-// 			continue ;
-// 		}
-// 		env_name = trim_env_name(args[i], env_operator);
-// 		if (!env_name && is_identifier(args[i]))
-// 		{
-// 			printf("args[%d]: %s check --B\n", i, args[i]);
-// 			return (0);
-// 		}
-// 		env_value = trim_env_value(args[i]);
-// 		if (!is_identifier(env_name) || (!env_name && !is_identifier(args[i]))
-// 			|| env_operator == INVALID_OP)
-// 		{
-// 			printf("args[%d]: %s check --C\n", i, args[i]);
-// 			g_status = GENERAL_ERRORS;
-// 			return (command_error(args[i], "export"));
-// 		}
-// 		printf("args[%d]: %s check --OK\n", i, args[i]);
-// 		handle_env_operation(env_map, env_operator, env_name, env_value);
-// 		i++;
-// 	}
-// 	return (g_status);
-// }

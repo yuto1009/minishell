db8d10bcaa346b287bd881f10ccb3218d9ba9b64
Kazushi Yoshida <kyoshida@c2r3s7.42tokyo.jp>
2024-06-01 17:01:27 +0900

    freeした

--- built_in/builtin_pwd.c
+++ built_in/builtin_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_pwd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yuendo <yuendo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kyoshida <kyoshida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:29:17 by yutoendo          #+#    #+#             */
-/*   Updated: 2024/06/01 16:35:48 by yuendo           ###   ########.fr       */
+/*   Updated: 2024/06/01 16:52:47 by kyoshida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	builtin_pwd(t_var *env)
 	if (getcwd(cwd, PATH_MAX) == NULL)
 	{
 		ans = get_env_value(pwd ,env);
-		printf("%s\n",ans);    
+		printf("%s\n",ans); 
+		free(ans);   
 		// printf("hogeghoge\n");
 		return 0;
 	}
